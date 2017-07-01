631e2cc68d91b7bf4da8243a6a9c1d907c49b3eb
SUDIKSHACHANDEL <Sudikshachandel15112@gmail.com>
2017-07-01 12:52:22 +0530

    this is my spy_detail file

---
+++ spy_details.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+spy = {
+  'name': 'Sudiksha',
+  'salutation': 'Ms.',
+  'age': 20,
+  'rating': 4.7,
+  'is_online': True
+}
+
+
+
+
+
+friends = [
+    {
+        'name': 'Jyoti',
+        'saultation': 'Ms.',
+        'rating': 389,
+        'age': 21,
+        'chats': []
+    },
+    {
+        'name': 'Savita',
+        'saultation': 'Ms.',
+        'rating': 5.9,
+        'age': 23,
+        'chats': []
+    }
+
+]
